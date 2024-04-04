a81b47ec770d45567fb876a5b3fd623bfa082b6a
Mouinbhm <mouinbelhaj@icloud.com>
2024-04-04 18:41:39 +0100

    @include

---
+++ resources/views/include/admin/sidebar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-light navbar-vertical navbar-vibrant navbar-expand-lg">
+    <div class="collapse navbar-collapse" id="navbarVerticalCollapse">
+        <div class="navbar-vertical-content scrollbar">
+            <ul class="navbar-nav flex-column" id="navbarVerticalNav">
+                <li class="nav-item"><a class="nav-link active" href="/admin/categories">
+                        <div class="d-flex align-items-center"><span class="nav-link-icon"><span
+                                    data-feather="cast"></span></span><span class="nav-link-text">Categories</span>
+                        </div>
+                    </a>
+                </li>
+
+                <li class="nav-item"><a class="nav-link active" href="index.html">
+                        <div class="d-flex align-items-center"><span class="nav-link-icon"><span
+                                    data-feather="cast"></span></span><span class="nav-link-text">Produits</span></div>
+                    </a>
+                </li>
+                <li class="nav-item"><a class="nav-link active" href="index.html">
+                        <div class="d-flex align-items-center"><span class="nav-link-icon"><span
+                                    data-feather="cast"></span></span><span class="nav-link-text">Client</span></div>
+                    </a>
+                </li>
+
+
+            </ul>
+        </div>
+        <div class="navbar-vertical-footer"><a class="btn btn-link border-0 fw-semi-bold d-flex ps-0"
+                href="#!"><span class="navbar-vertical-footer-icon"
+                    data-feather="log-out"></span><span>Settings</span></a></div>
+    </div>
+</nav>
